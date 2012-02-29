47fc7beb5812f3aed130a297831eeb056432a775
Robby Walker <robbyw@greplin.com>
2012-02-29 04:30:43 -0800

    Collector that returns the first matching doc id

---
+++ src/main/java/com/greplin/lucene/search/AnyDocIdCollector.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2012 Greplin, Inc. All Rights Reserved.
+ */
+
+package com.greplin.lucene.search;
+
+import java.io.IOException;
+
+/**
+ * Determines if there were *any* matches to a query, returning the doc id.
+ */
+public class AnyDocIdCollector extends UnorderedCollector {
+  /**
+   * Stores the first matched document id.
+   */
+  private Integer hit = null;
+
+
+  /**
+   * @return the first matched document id, or null if none matched
+   */
+  public final Integer getHit() {
+    return hit;
+  }
+
+
+  @Override
+  public final void collect(final int doc) throws IOException {
+    if (hit == null) {
+      hit = getCurrentDocBase() + doc;
+    }
+  }
+}
